69e1cb9a576d983f75f68d4ffcf5476adf361dd5
LoayIssa <issaloay680@gmail.com>
2021-05-11 15:48:39 +0300

    solved q4 and q5 in puls check

--- app.js
+++ app.js
@@ -7,22 +7,52 @@ const port = 3000;
 
 app.use(express.json());
 
-let content;
-fs.readFile("./data.txt", (err, data) => {
-  if (err) {
-    console.log("ERR");
-  }
-  content = data.toString();
-  console.log(content);
-});
+const readFile =()=>{
+  let content;
+  fs.readFile("./data.txt", (err, data) => {
+    if (err) {
+      console.log("ERR");
+    }
+    content = data.toString();
+    console.log(content);
+  });
+}
 
 
 
-fs.writeFile("data.txt", "A new file has been created", (err) => {
-  if (err) {
-    console.log("ERR");
-  }
-});
+const writeFile =()=>{
+
+  fs.writeFile("data.txt", "A new file has been created", (err) => {
+    if (err) {
+      console.log("ERR");
+    }
+  });
+}
+
+
+
+const getPost = (id) => {
+  axios
+  .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  .then((response) => {
+    console.log(response.data);
+    res.json(response.data);
+
+  })
+  .catch((err) => {
+   console.log("ERR")
+  });
+};
+
+
+const getPostAsync = async (data) => {
+    try {
+        const response = await  axios.get(`https://jsonplaceholder.typicode.com/posts/${data}/`)
+        console.log(response.data)
+    }catch (err) {
+        throw err;
+      }
+}; 
 
 app.listen(port, () => {
   console.log(`practice app listening at http://localhost:${port}`);
